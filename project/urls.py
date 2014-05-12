0ed4bb538c2d07a3b5833f121868368235d43a22
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2014-05-12 22:18:11 +0200

    Hello, world!

--- project/urls.py
+++ project/urls.py
@@ -5,8 +5,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'project.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+    url(r'^$', include('hello.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
 )
