710c723c5fa00c9dc79ad4ab4cfcf35796591947
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2014-07-17 09:30:09 +0200

    user and keywords for notes

--- hello_blog/models.py
+++ hello_blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
@@ -10,7 +11,9 @@ class Category(models.Model):
         return self.name
 
 class Note(models.Model):
+    user = models.ForeignKey(User)
     category = models.ForeignKey(Category)
+    keywords = models.CharField(max_length=400)
     title = models.CharField(max_length=200)
     date = models.DateTimeField()
     content = models.TextField(max_length=50000)
