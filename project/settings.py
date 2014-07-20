7acb3df2714da8829c6447bcedca2e7111a46b3d
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2014-07-20 19:28:46 +0200

    SECRET_KEY always from secret.txt

--- project/settings.py
+++ project/settings.py
@@ -104,19 +104,16 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(SITE_DIR, 'media')
 
 
+SECRET_FILE = os.path.join(PROJECT_DIR, 'secret.txt')
 try:
-    SECRET_KEY
-except NameError:
-    SECRET_FILE = os.path.join(PROJECT_DIR, 'secret.txt')
+    SECRET_KEY = open(SECRET_FILE).read().strip()
+except IOError:
     try:
-        SECRET_KEY = open(SECRET_FILE).read().strip()
+        import random
+        SECRET_KEY = ''.join([random.SystemRandom().choice('abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)') for i in range(50)])
+        secret = file(SECRET_FILE, 'w')
+        secret.write(SECRET_KEY)
+        secret.close()
     except IOError:
-        try:
-            import random
-            SECRET_KEY = ''.join([random.SystemRandom().choice('abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)') for i in range(50)])
-            secret = file(SECRET_FILE, 'w')
-            secret.write(SECRET_KEY)
-            secret.close()
-        except IOError:
-            Exception('Please create a %s file with random characters \
-            to generate your secret key!' % SECRET_FILE)
+        Exception('Please create a %s file with random characters \
+        to generate your secret key!' % SECRET_FILE)
