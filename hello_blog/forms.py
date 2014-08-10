25ecd5f78ae66ded5c5deffc6c91081f032955c5
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2014-08-10 19:51:26 +0200

    Custom operator (AND, OR)

--- hello_blog/forms.py
+++ hello_blog/forms.py
@@ -1,6 +1,14 @@
+from django import forms
 from haystack.forms import HighlightedModelSearchForm
+from haystack.inputs import Raw, Clean
+from haystack.query import SearchQuerySet
 
 class BlogSearchForm(HighlightedModelSearchForm):
+    operator = forms.ChoiceField(choices=(('AND', 'AND'),('OR', 'OR'),))
+
     def search(self):
-        qs = super(BlogSearchForm, self).search()
-        return qs
+        sqs = SearchQuerySet().filter(content=Raw(Clean((' %s ' % self.cleaned_data['operator']).join(self.cleaned_data['q'].split(' ')))))
+        print(unicode(sqs.query))
+        if self.cleaned_data['models']:
+            sqs = sqs.models(*self.get_models())
+        return sqs
