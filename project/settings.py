6804f6109c79c779785f5c2bb638249b2ebe03e8
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2014-08-16 17:22:24 +0200

    Polish LANGUAGE_CODE and TIME_ZONE

--- project/settings.py
+++ project/settings.py
@@ -56,9 +56,9 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pl'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Warsaw'
 
 USE_I18N = True
 
