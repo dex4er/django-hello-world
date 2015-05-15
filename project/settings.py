ed538dcb17804c87cd435295a59ad248930abe43
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2015-05-15 15:41:27 +0200

    USE_X_FORWARDED_HOST=True, new config settings_secret, no warnings

--- project/settings.py
+++ project/settings.py
@@ -8,18 +8,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.6/ref/settings/
 """
 
-from settings_local import *
-from settings_dirs import *
-from settings_db import *
-from settings_email import *
-from settings_log import *
-
-
-import os
-from distutils.sysconfig import get_python_lib
-
-# /home/app/site/virtualenv
-PYTHON_LIB_DIR = get_python_lib()
+from settings_local import *  # @UnusedWildImport
+from settings_dirs import *  # @UnusedWildImport
+from settings_db import *  # @UnusedWildImport
+from settings_email import *  # @UnusedWildImport
+from settings_log import *  # @UnusedWildImport
+from settings_secret import *  # @UnusedWildImport
 
 
 # Application definition
@@ -85,6 +79,29 @@ STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
 )
 
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
+    'apptemplates.Loader',
+    # 'django.template.loaders.eggs.Loader',
+)
+
+TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.contrib.auth.context_processors.auth',
+    'django.core.context_processors.debug',
+    'django.core.context_processors.i18n',
+    'django.core.context_processors.media',
+    'django.core.context_processors.static',
+    'django.core.context_processors.tz',
+    'django.contrib.messages.context_processors.messages',
+    'django.core.context_processors.request',
+)
+
+TEMPLATE_DIRS = (
+    BASE_DIR + '/templates',
+    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
+)
+
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 
@@ -93,20 +110,5 @@ MEDIA_URL = '/media/'
 # /home/app/site/media
 MEDIA_ROOT = os.path.join(SITE_DIR, 'media')
 
-
-try:
-    SECRET_KEY
-except NameError:
-    SECRET_FILE = os.path.join(PROJECT_DIR, 'secret.txt')
-    try:
-        SECRET_KEY = open(SECRET_FILE).read().strip()
-    except IOError:
-        try:
-            import random
-            SECRET_KEY = ''.join([random.SystemRandom().choice('abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)') for i in range(50)])
-            secret = file(SECRET_FILE, 'w')
-            secret.write(SECRET_KEY)
-            secret.close()
-        except IOError:
-            Exception('Please create a %s file with random characters \
-            to generate your secret key!' % SECRET_FILE)
+# Revproxy
+USE_X_FORWARDED_HOST = True
