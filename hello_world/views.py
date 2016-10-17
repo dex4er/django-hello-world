f849edf782277a348f5c977cc231185e6540b011
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2016-10-17 11:29:21 +0200

    Output HTML so toolbar can be shown

--- hello_world/views.py
+++ hello_world/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world!")
+    return HttpResponse("<html><head></head><body>Hello, world!</body></html>")
