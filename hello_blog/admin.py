8c0ab795a0cc4a5c7d7a366d4f84b848571f124e
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2018-07-19 20:31:28 +0200

    @admin.register

--- hello_blog/admin.py
+++ hello_blog/admin.py
@@ -3,13 +3,11 @@ from django.contrib import admin
 import hello_blog.models
 
 
+@admin.register(hello_blog.models.Category)
 class CategoryAdmin(admin.ModelAdmin):
     pass
 
-admin.site.register(hello_blog.models.Category, CategoryAdmin)
 
-
+@admin.register(hello_blog.models.Note)
 class NoteAdmin(admin.ModelAdmin):
     pass
-
-admin.site.register(hello_blog.models.Note, NoteAdmin)
