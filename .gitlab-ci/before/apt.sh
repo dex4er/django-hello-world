d7b15fc380570f627739d0159e16eaa18c6e88c9
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2019-09-02 09:56:00 +0200

    ci: apt with mirror

--- .gitlab-ci/before/apt.sh
+++ .gitlab-ci/before/apt.sh
@@ -4,4 +4,12 @@ APT::Get::Fix-Broken "true";
 APT::Install-Recommends "false";
 END
 
+. /etc/lsb-release
+
+cat << END > /etc/apt/sources.list
+deb ${APT_URL:-mirror://mirrors.ubuntu.com/mirrors.txt} ${DISTRIB_CODENAME} main restricted universe
+deb ${APT_URL:-mirror://mirrors.ubuntu.com/mirrors.txt} ${DISTRIB_CODENAME}-updates main restricted universe
+deb ${APT_URL:-mirror://mirrors.ubuntu.com/mirrors.txt} ${DISTRIB_CODENAME}-security main restricted universe
+END
+
 apt-get update
